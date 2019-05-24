e819b7236b7863a5fb18bdff7d5c15896a4b9c3a
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-05-24 12:54:25 +0100

    Add tests for github api

--- src/components/Profile.test.tsx
+++ src/components/Profile.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer'
+import axios from 'axios'
+import moxios from 'moxios'
 import Profile from './Profile';
 
 describe('Profile component', function() {
+    beforeAll(() => {
+        moxios.install();
+    });
+
+    afterAll(() => {
+        moxios.uninstall();
+    });
+
     it('renders without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(<Profile userName="test" />, div);
@@ -16,5 +26,18 @@ describe('Profile component', function() {
         );
 
         expect(component.root.props.userName).toBe('test');
+    });
+
+    it('fetch the correct user details', async () => {
+        moxios.stubRequest('https://api.github.com/users/test', {
+            status: 200,
+            responseText: {
+                name: "myName"
+            }
+        });
+
+        const response = await axios.get('https://api.github.com/users/test');
+        expect(response.status).toBe(200);
+        expect(response.data.name).toBe("myName");
     });
 })
