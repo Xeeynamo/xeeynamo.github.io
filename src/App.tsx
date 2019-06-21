0fc832e011a4e49ffe084971ae6b4f1ff620e36f
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-06-21 14:14:49 +0100

    Remove React hello world

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,12 @@ import config from './config.json'
 const App: React.FC = () => {
   return (
     <div className="App">
-      <MasterProfile userName={config.userName}/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Work in progress
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <MasterProfile userName={config.userName}/>
     </div>
   );
 }
