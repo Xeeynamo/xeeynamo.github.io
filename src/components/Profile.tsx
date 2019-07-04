74feae6867a9d4a7b0e096bc09bcbfebb6537b1e
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-07-04 16:27:03 +0100

    Add Material-UI on Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Profile.css'
+import { Theme, createStyles, makeStyles, useTheme } from '@material-ui/core/styles';
+import { Card, CardContent, Typography, CardMedia, Link } from '@material-ui/core';
+import { openLink, ga } from '../services/Utilities';
 
 interface Props {
     name: string;
@@ -9,16 +12,86 @@ interface Props {
     fullName: string;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        card: {
+            display: 'flex',
+            marginTop: "20px",
+            marginBottom: "20px",
+            marginLeft: "auto",
+            marginRight: "auto",
+            maxWidth: "700px",
+            textAlign: "center"
+        },
+        details: {
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        content: {
+            flex: '1 0 auto',
+        },
+        cover: {
+            width: 230,
+            height: 230,
+            backgroundSize: "contain"
+        },
+        controls: {
+            display: 'flex',
+            alignItems: 'center',
+            paddingLeft: theme.spacing(1),
+            paddingBottom: theme.spacing(1),
+        },
+        playIcon: {
+            height: 38,
+            width: 38,
+        },
+    }),
+);
+
 export function Profile(props: Props) {
+    const classes = useStyles();
+
     return (
-        <div className="profile">
-            <div>
-                <img src={props.imageUrl} width={100} alt="logo" />
+        <Card className={classes.card}>
+            <CardMedia
+                className={classes.cover}
+                image={props.imageUrl}
+                title={props.fullName}
+            />
+            <div className={classes.details}>
+                <CardContent className={classes.content}>
+                    <div>
+                        <Link
+                            component="button"
+                            variant="body2"
+                            onClick={() => {
+                                ga.homepageClick("GitHub profile");
+                                ga.behaviour("Full name", "GitHub profile")
+                                openLink(props.homeUrl)
+                            }}
+                        >
+                            <Typography component="h5" variant="h5">
+                                {props.fullName}
+                            </Typography>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link
+                            component="button"
+                            variant="body2"
+                            onClick={() => {
+                                ga.homepageClick("GitHub profile");
+                                ga.behaviour("User name", "GitHub profile")
+                                openLink(props.homeUrl)
+                            }}
+                        >
+                            <Typography variant="subtitle1" color="textSecondary">
+                                {props.name}
+                            </Typography>
+                        </Link>
+                    </div>
+                </CardContent>
             </div>
-            <div>
-                <a href={props.homeUrl}>{props.name}</a>
-                <div>{props.fullName}</div>
-            </div>
-        </div>
-    )
+        </Card>
+    );
 }
