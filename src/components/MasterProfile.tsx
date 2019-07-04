349a44825a85ef031b58ef707447e05b9fdc9525
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-07-04 10:28:49 +0100

    Move user fetch from Profile to MasterProfile

--- src/components/MasterProfile.tsx
+++ src/components/MasterProfile.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { octokit } from '../services/Octokit'
 import { Profile } from './Profile';
 import { RepositoryList } from './RepositoryList';
 
@@ -6,15 +7,66 @@ interface Props {
     userName: string
 }
 
-interface State {
+interface User {
+    name: string
+    id: number
+    imageUrl: string
+    homeUrl: string
+    fullName: string
+}
 
+interface State {
+    user?: User
 }
 
 export class MasterProfile extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+
+        this.state = {
+            user: undefined
+        }
+
+        this.fetchUser();
+    }
+
+    private async fetchUser() {
+        const response = await octokit.users.getByUsername({
+            username: this.props.userName
+        })
+
+        this.setState({
+            user: {
+                name: response.data.login,
+                id: response.data.id,
+                imageUrl: response.data.avatar_url,
+                homeUrl: response.data.html_url,
+                fullName: response.data.name
+            }
+        })
+
+        return response.data
+    }
+
+    private renderUser() {
+        if (this.state.user === undefined)
+            return
+
+        return (
+            <Profile
+                name={this.state.user.name}
+                id={this.state.user.id}
+                imageUrl={this.state.user.imageUrl}
+                homeUrl={this.state.user.homeUrl}
+                fullName={this.state.user.fullName}
+            />
+        )
+    }
+
     render() {
         return (
             <div>
-                <Profile userName={this.props.userName} />
+                {this.renderUser()}
                 <RepositoryList userName={this.props.userName} />
             </div>
         );
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,10 +3,6 @@ import { octokit } from '../services/Octokit'
 import './Profile.css'
 
 interface Props {
-    userName: string
-}
-
-interface State {
     name: string;
     id: number;
     imageUrl: string;
@@ -14,43 +10,24 @@ interface State {
     fullName: string;
 }
 
+interface State {
+
+}
+
 export class Profile extends Component<Props, State> {
     constructor(props: Props) {
         super(props);
-        this.state = {
-            name: "foo",
-            id: 0,
-            imageUrl: "",
-            homeUrl: "",
-            fullName: "",
-        };
-
-        this.fetchUser(this.props.userName);
-    }
-
-    private async fetchUser(userName: string) {
-        const response = await octokit.users.getByUsername({
-            username: userName
-        })
-
-        this.setState({
-            name: response.data.login,
-            id: response.data.id,
-            imageUrl: response.data.avatar_url,
-            homeUrl: response.data.html_url,
-            fullName: response.data.name
-        })
     }
 
     render() {
         return (
             <div className="profile">
                 <div>
-                    <img src={this.state.imageUrl} width={100} alt="logo" />
+                    <img src={this.props.imageUrl} width={100} alt="logo" />
                 </div>
                 <div>
-                    <a href={this.state.homeUrl}>{this.state.name}</a>
-                    <div>{this.state.fullName}</div>
+                    <a href={this.props.homeUrl}>{this.props.name}</a>
+                    <div>{this.props.fullName}</div>
                 </div>
             </div>
         );
