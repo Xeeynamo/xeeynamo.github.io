2457b708d9b50a6b91ddab5d498f916a5ce0747a
Luciano Ciccariello <xeeynamo@hotmail.com>
2019-07-23 10:40:55 +0100

    Improve performance of ApplauseButton tests

--- src/components/ApplauseButton.test.tsx
+++ src/components/ApplauseButton.test.tsx
@@ -6,10 +6,6 @@ import { ApplauseButton } from './ApplauseButton';
 import { delay } from 'q';
 
 describe('Applause button component', () => {
-    const sleepTime = 50;
-    const sleepTimeForStateUpdate = 50;
-    const sleepTimeForPostCall = 50;
-    const sleepTimeForBufferTime = 50;
     const myBufferTime = 20;
 
     beforeAll(() => {
@@ -54,8 +50,10 @@ describe('Applause button component', () => {
 
         it("updates the clap count with the server-returned value", async () => {
             const wrapper = mount(<ApplauseButton />);
-            await delay(sleepTime);
-            expect(wrapper.find(".count").text()).toEqual("44");
+
+            moxios.wait(() => {
+                expect(wrapper.find(".count").text()).toEqual("44");
+            })
         });
 
         it("initialises in loading state", () => {
@@ -65,8 +63,10 @@ describe('Applause button component', () => {
 
         it("removes loading state when clap count retrieved", async () => {
             const wrapper = shallow(<ApplauseButton />);
-            await delay(sleepTime);
-            expect(wrapper.find(".applause-button").hasClass("loading")).toBeFalsy();
+
+            moxios.wait(() => {
+                expect(wrapper.find(".applause-button").hasClass("loading")).toBeFalsy();
+            })
         });
 
         it("is not in clapped state", () => {
@@ -81,13 +81,14 @@ describe('Applause button component', () => {
 
         it("get claps from custom url", async () => {
             const wrapper = mount(<ApplauseButton url='/custom-url' />);
-            await delay(sleepTime);
 
-            const request = moxios.requests.mostRecent();
-            expect(request.config.url).toBe("https://api.applause-button.com/get-claps?url=/custom-url");
-            expect(request.config.method).toBe("get");
+            moxios.wait(() => {
+                const request = moxios.requests.mostRecent();
+                expect(request.config.url).toBe("https://api.applause-button.com/get-claps?url=/custom-url");
+                expect(request.config.method).toBe("get");
 
-            expect(wrapper.find(".count").text()).toEqual("123");
+                expect(wrapper.find(".count").text()).toEqual("123");
+            })
         });
     })
 
@@ -103,31 +104,34 @@ describe('Applause button component', () => {
         it("updates clapped state", async () => {
             const wrapper = shallow(<ApplauseButton />);
             clickTheButton(wrapper);
-            await delay(sleepTimeForStateUpdate);
+            await delay(50);
+
             expect(wrapper.hasClass("clapped")).toBeTruthy();
         });
 
         it("send the updated clap count to the server", async () => {
             const wrapper = shallow(<ApplauseButton bufferTimer={myBufferTime} />);
             clickTheButton(wrapper);
-            await delay(sleepTimeForPostCall);
 
-            const request = moxios.requests.mostRecent();
-            expect(request.config.url).toBe("https://api.applause-button.com/update-claps");
-            expect(request.config.method).toBe("post");
-            expect(request.config.data).toEqual('"1,3.3.0"');
+            moxios.wait(() => {
+                const request = moxios.requests.mostRecent();
+                expect(request.config.url).toBe("https://api.applause-button.com/update-claps");
+                expect(request.config.method).toBe("post");
+                expect(request.config.data).toEqual('"1,3.3.0"');
+            })
         });
 
         it("ensure that only a single clap is registered if not in multiclap mode", async () => {
             const wrapper = shallow(<ApplauseButton multiClap={false} bufferTimer={myBufferTime} />);
             clickTheButton(wrapper);
             clickTheButton(wrapper);
-            await delay(sleepTimeForBufferTime);
 
-            const request = moxios.requests.mostRecent();
-            expect(request.config.url).toBe("https://api.applause-button.com/update-claps");
-            expect(request.config.method).toBe("post");
-            expect(request.config.data).toEqual('"1,3.3.0"');
+            moxios.wait(() => {
+                const request = moxios.requests.mostRecent();
+                expect(request.config.url).toBe("https://api.applause-button.com/update-claps");
+                expect(request.config.method).toBe("post");
+                expect(request.config.data).toEqual('"1,3.3.0"');
+            })
         });
 
         it("ensure the max clap count is not exceeded when multiclapping", async () => {
@@ -135,12 +139,12 @@ describe('Applause button component', () => {
             for (let i = 0; i < 15; i++)
                 clickTheButton(wrapper);
 
-            await delay(sleepTimeForBufferTime);
-
-            const request = moxios.requests.mostRecent();
-            expect(request.config.url).toBe("https://api.applause-button.com/update-claps");
-            expect(request.config.method).toBe("post");
-            expect(request.config.data).toEqual('"10,3.3.0"');
+            moxios.wait(() => {
+                const request = moxios.requests.mostRecent();
+                expect(request.config.url).toBe("https://api.applause-button.com/update-claps");
+                expect(request.config.method).toBe("post");
+                expect(request.config.data).toEqual('"10,3.3.0"');
+            })
         });
 
         it("Ensure to set clap-limit-exceeded class when max clap count is reached", async () => {
@@ -148,9 +152,9 @@ describe('Applause button component', () => {
             for (let i = 0; i < 15; i++)
                 clickTheButton(wrapper);
 
-            await delay(sleepTimeForStateUpdate);
-
-            expect(wrapper.find(".applause-button").hasClass("clap-limit-exceeded")).toBeTruthy();
+            moxios.wait(() => {
+                expect(wrapper.find(".applause-button").hasClass("clap-limit-exceeded")).toBeTruthy();
+            })
         })
     })
 })
