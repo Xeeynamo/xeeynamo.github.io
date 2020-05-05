fad2b8eba7930f31142e4e529325beb4d11669da
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-05-05 07:40:56 +0000

    Updates

---
+++ precache-manifest.3559b40d8b1e6b00124d009da4b043de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d64235278050b95c8d304dffc80947",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f815584986153dbae496",
+    "url": "/static/css/main.3510806e.chunk.css"
+  },
+  {
+    "revision": "46f8329276cc629e31d0",
+    "url": "/static/js/2.4e2cc2b8.chunk.js"
+  },
+  {
+    "revision": "f815584986153dbae496",
+    "url": "/static/js/main.139f0d80.chunk.js"
+  },
+  {
+    "revision": "baafe5c647ae515632e5",
+    "url": "/static/js/runtime-main.b10308a3.js"
+  }
+]);
