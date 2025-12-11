416643c07573f23f65ea8b15a5d0b658c526570c
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-12-11 15:22:35 +0000

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,18 @@ import me from '../data/me'
 
 export default function Header() {
   return (
-    <div className='Header'>
+    <header className='Header'>
       <h1>{me.name}</h1>
       <h2>{me.title}</h2>
-      <nav>
-        {me.links.map(link => (
-          <a key={link.url} href={link.url}>{link.name}</a>
-        ))}
-      </nav>
       <nav>
         <a href="#about">About</a>
         <a href="#work">Work</a>
         <a href="#projects">Projects</a>
+        <span className="nav-separator">|</span>
+        {me.links.map(link => (
+          <a key={link.url} href={link.url}>{link.name}</a>
+        ))}
       </nav>
-    </div>
+    </header>
   );
 }
